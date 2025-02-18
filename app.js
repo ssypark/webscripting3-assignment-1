aca106fe641446d981021fa04101503f2c7d38eb
hobbybill <sungyool.park@gmail.com>
2025-02-17 19:59:43 -0800

    commit init

--- app.js
+++ app.js
@@ -7,8 +7,13 @@ const app = express();
 // Set the port to 3001
 const PORT = 3001;
 
+//To use images in the public folder we need to use the express.static middleware
+// static middleware is used to serve static files such as images, CSS files, and JavaScript files
+app.use(express.static('public'));
+
 // Here, we are importing the readingsRouter from the routes/readings.js file
 const readingsRouter = require('./routes/readings');
+
 // the bodyParser module is used to parse incoming requests as a middleware
 const bodyParser = require('body-parser');
 
